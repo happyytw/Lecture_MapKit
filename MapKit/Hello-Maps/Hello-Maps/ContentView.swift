d3c16e4742f46d8c3d40cd803276e2f1eff2b392
happyytw <2000ytw@naver.com>
2023-10-09 12:51:47 +0900

    수정

--- MapKit/Hello-Maps/Hello-Maps/ContentView.swift
+++ MapKit/Hello-Maps/Hello-Maps/ContentView.swift
@@ -6,12 +6,14 @@ struct ContentView: View {
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)// 지도에서 카메라의 시선이 어디로 가야하는지를 지정한다.
     @State private var locationManager = LocationManager.shared // 기존에는 StateObject를 사용했지만 @Observable 메크로를 사용하여 @State라고만 명시하면 된다.
     @State private var selectedMapOption: MapOptions = .standard
+    @State private var query: String = ""
+    @State private var selectedDetent: PresentationDetent = .fraction(0.15)
     
     var body: some View {
         
-        ZStack(alignment: .top) {
+        ZStack(alignment: .top) { // layer,위 아래로 뷰를 설정할 수 있다.
             
-            Map(position: $position) {
+            Map(position: $position) { // 지도 생성
                 Annotation("Coffe", coordinate: .coffee) {
                     Image(systemName: "cup.and.saucer.fill")
                         .padding(4)
@@ -27,6 +29,21 @@ struct ContentView: View {
                 UserAnnotation() // 내 위치 표현
                 
             }
+            .sheet(isPresented: .constant(true), content: {
+                VStack {
+                    TextField("Search", text: $query)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        .onSubmit {
+                            // code fired when you return in TextField
+                        }
+                    Spacer()
+                }
+                    .presentationDetents([.fraction(0.15), .medium, .large], selection: $selectedDetent) // fraction:커스텀 높이
+                    .presentationDragIndicator(.visible) // 드래그할 수 있는게 표시된다.
+                    .interactiveDismissDisabled() // 사용자가 직업 없애는걸 막아준다.
+                    .presentationBackgroundInteraction(.enabled(upThrough: .medium)) // 중간 위로부터는 시트 뒤에 있는 배경과 상호작용이 가능해진다.
+            })
             .mapControls({
                 MapUserLocationButton()
                 MapCompass()
